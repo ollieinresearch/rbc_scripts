d72da16f14abc00fa4fac446eddf0cc2449c28cc
Oliver Brown <ollietheengineer@uvic.ca>
2025-07-28 04:44:53 -0400

    fixed sim_ready.sh

--- rorqual/2d/sim_ready.sh
+++ rorqual/2d/sim_ready.sh
@@ -1,2 +1,2 @@
-cp -f /project/def-goluskin/ollie/scripts/2d/*.sh .
-cp -f /project/def-goluskin/ollie/scripts/*.sh .
+cp -f /home/ollie/links/scratch/rbc_scripts/rorqual/2d/*.sh .
+cp -f /home/ollie/links/scratch/rbc_scripts/rorqual/*.sh .
--- rorqual/3d/sim_ready.sh
+++ rorqual/3d/sim_ready.sh
@@ -1,2 +1,3 @@
-cp -f /project/def-goluskin/ollie/scripts/3d/*.sh .
-cp -f /project/def-goluskin/ollie/scripts/*.sh .
+cp -f /home/ollie/links/scratch/rbc_scripts/rorqual/3d/visualization/*.sh .
+cp -f /home/ollie/links/scratch/rbc_scripts/rorqual/3d/*.sh .
+cp -f /home/ollie/links/scratch/rbc_scripts/rorqual/*.sh .
