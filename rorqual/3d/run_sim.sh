58965f9e3c9c1c0086575d6b1dfad003c12377bf
Oliver Brown <ollietheengineer@uvic.ca>
2025-07-28 03:10:09 -0400

    fixed run_sim script for rorqual

--- rorqual/3d/run_sim.sh
+++ rorqual/3d/run_sim.sh
@@ -2,49 +2,14 @@
 
 #SBATCH --output=job_sim.out
 #SBATCH --time=11:59:00
-#SBATCH --nodes=4
+#SBATCH --nodes=6
 #SBATCH --ntasks-per-node=96
 #SBATCH --mem=0
 #SBATCH --job-name=r1e7_pr3e-2
-
 #SBATCH --account=def-goluskin
-
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=ollietheengineer@uvic.ca
 
-# Load the required modules
-module purge
-module load StdEnv/2020
-module load python/3.10.2 mpi4py fftw-mpi hdf5-mpi 
-
-# Dedalus performance tip!
-export OMP_NUM_THREADS=1
-export NUMEXPR_MAX_THREADS=1
-
-# For our virtual environment
-env=$SLURM_TMPDIR/env
-
-# path to all python scripts for simulations; change as needed
-PATH_TO_SCRIPTS="/project/def-goluskin/ollie/scripts/3d"
-PATH_TO_GEN_SCRIPTS="/project/def-goluskin/ollie/scripts"
-
-# Create the virtual environment on each node: 
-srun --ntasks $SLURM_NNODES --tasks-per-node=1 bash << EOF
-virtualenv --no-download $env
-source $env/bin/activate
-
-pip install --no-index --upgrade pip
-pip install --no-index -r $PATH_TO_GEN_SCRIPTS/requirements.txt
-EOF
-
-
-# Required for multiple node simulations
-source $env/bin/activate;
-
-# Run Dedalus RB script in 3D with specified parameters.
-# Can either use an initial condition or start fresh.
-# Merge files for analysis.
-# Run analysis script to produce info and plots.
 
 ################################################################################
 # User specified parameters
@@ -69,16 +34,53 @@ LY=2
 # you have a grid of size Lx*R x Ly*R x R, and if your mesh is of size [n,m], then on each core you compute
 # (Lx*R/n)x(Ly*R/m) pencils of length R.
 MESHX=24
-MESHY=16
+MESHY=24
 
 # Use initial condition? (1=yes, 0=no)
 IC=1
 ################################################################################
 
 
+# Load the required modules
+module purge
+module load StdEnv/2020
+module load python/3.10.2 mpi4py fftw-mpi hdf5-mpi 
+
+# Dedalus performance tip!
+export OMP_NUM_THREADS=1
+export NUMEXPR_MAX_THREADS=1
+
+# For our virtual environment
+env=$SLURM_TMPDIR/env
+
+# path to all python scripts for simulations; change as needed
+PATH_TO_SCRIPTS="/home/ollie/links/scratch/rbc_scripts"
+SCRIPTS_3D="/home/ollie/links/scratch/rbc_scripts/3d"
+
+# Create the virtual environment on each node: 
+srun --ntasks $SLURM_NNODES --tasks-per-node=1 bash << EOF
+virtualenv --no-download $env
+source $env/bin/activate
+
+pip install --no-index --upgrade pip
+pip install --no-index -r $PATH_TO_SCRIPTS/requirements.txt
+EOF
+
+
+# Required for multiple node simulations
+source $env/bin/activate;
+
+# Run Dedalus RB script in 3D with specified parameters.
+# Can either use an initial condition or start fresh.
+# Merge files for analysis.
+# Run analysis script to produce info and plots.
+
+
+
+
 # Specify desired time for initial condition - 0 implies most recent
 if [ $IC -eq 1 ]; then
-  IC_ARRAY=($(python3 $PATH_TO_GEN_SCRIPTS/initial_condition.py 0 --file=$PWD/restart/restart.h5))
+  IC_ARRAY=($(python3 $PATH_TO_SCRIPTS/initial_condition.py 0 --file=$PWD/restart/restart.h5))
   TOTAL_TIME=$(echo "$SIM_TIME+${IC_ARRAY[1]}" | bc)
   IND=${IC_ARRAY[0]}
 else
@@ -89,7 +91,7 @@ else
   IND=-1
 fi
 
-srun python3 $PATH_TO_SCRIPTS/rayleigh_benard_script3d.py --Ra=$RA --Pr_exp=$PR_EXP --res=$RES --dt=$DT --sim_time=$TOTAL_TIME --basepath=$PWD --stepper=$STEPPER --Lx=$LX --Ly=$LY --meshx=$MESHX --meshy=$MESHY --cfl
+srun python3 $SCRIPTS_3D/rayleigh_benard_script3d.py --Ra=$RA --Pr_exp=$PR_EXP --res=$RES --dt=$DT --sim_time=$TOTAL_TIME --basepath=$PWD --stepper=$STEPPER --Lx=$LX --Ly=$LY --meshx=$MESHX --meshy=$MESHY --cfl
 
 # Post processing
 if [ -f "field_analysis/field_analysis.h5" ]; then
@@ -121,12 +123,12 @@ fi
 rm -rf restart/restart.h5
 ln -sv $PWD/state/$RECENT $PWD/restart/restart.h5
 
-srun python3 $PATH_TO_GEN_SCRIPTS/analysis.py $PWD/analysis/analysis.h5 --time=0 --basepath=$PWD
+srun python3 $PATH_TO_SCRIPTS/analysis.py $PWD/analysis/analysis.h5 --time=0 --basepath=$PWD
 
 mkdir res_check
 mkdir res_check_3d
 
-srun python3 $PATH_TO_GEN_SCRIPTS/power.py $PWD/state/*.h5
-srun python3 $PATH_TO_SCRIPTS/3d/power.py $PWD/state/*.h5
+srun python3 $PATH_TO_SCRIPTS/power.py $PWD/state/*.h5
+srun python3 $SCRIPTS_3D/power.py $PWD/state/*.h5
 ffmpeg -y -r 15 -pattern_type glob -i 'res_check/*.png' -threads 40 -pix_fmt yuv420p res_check/movie.mp4
 ffmpeg -y -r 15 -pattern_type glob -i 'res_check_3d/*.png' -threads 40 -pix_fmt yuv420p res_check_3d/movie.mp4
